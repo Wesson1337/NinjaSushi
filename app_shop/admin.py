2bcd96e0ea80881cabe92b6792e5c25edae82ca5
Wesson <vinokurov.filipp1@yandex.ru>
2022-08-06 20:40:30 +0300

    filled cards, made some minor edits

--- app_shop/admin.py
+++ app_shop/admin.py
@@ -5,6 +5,7 @@ from app_shop.models import Product, Category
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['id', 'name', 'category']
+    list_filter = ['category']
 
 
 @admin.register(Category)
