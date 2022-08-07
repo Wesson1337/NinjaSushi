ae156bbfa109aa127dfc091c8945aa695632a4e4
Wesson <vinokurov.filipp1@yandex.ru>
2022-08-07 19:59:17 +0300

    finished filling cards, moved base.html, base.css, base img/svg into general dir

--- app_shop/models.py
+++ app_shop/models.py
@@ -15,7 +15,7 @@ class Category(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=255, verbose_name='название')
     amount = models.PositiveIntegerField(verbose_name='количество')
-    composition = models.TextField(default=None, verbose_name='состав')
+    composition = models.TextField(default=None, blank=True, verbose_name='состав')
     weight = models.PositiveIntegerField(verbose_name='вес')
     price = models.PositiveIntegerField(verbose_name='цена')
     image = models.ImageField(upload_to='img/app_shop/product_images', verbose_name='фото')
