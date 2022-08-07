cd74f06cc5efe8fa4ffe80f12239143ffe085d59
Wesson <vinokurov.filipp1@yandex.ru>
2022-08-07 21:07:13 +0300

    made main_page_detail.html, main_page_detail_view

--- app_shop/urls.py
+++ app_shop/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from app_shop.views import main_page_view
+from app_shop.views import main_page_view, main_page_detail_view
 
 urlpatterns = [
-    path('', main_page_view, name='main_page')
+    path('', main_page_view, name='main_page'),
+    path('<int:category_pk>', main_page_detail_view, name='main_page_detail')
 ]
--- app_shop/views.py
+++ app_shop/views.py
@@ -4,6 +4,20 @@ from app_shop.models import Product, Category
 
 
 def main_page_view(request):
+    """Renders main page"""
     products = Product.objects.select_related('category').all()
     categories = Category.objects.all().order_by('id')
     return render(request, 'app_shop/main_page.html', context={'products': products, 'categories': categories})
+
+
+def main_page_detail_view(request, category_pk):
+    """Renders main page detailed by category"""
+    products = Product.objects.filter(category=category_pk)
+    categories = Category.objects.all().order_by('id')
+    category = Category.objects.get(id=category_pk)
+    context = {
+        'products': products,
+        'categories': categories,
+        'category': category
+    }
+    return render(request, 'app_shop/main_page_detail.html', context=context)
