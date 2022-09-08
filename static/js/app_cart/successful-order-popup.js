ca9c82355067af9d0486e49426de81ec7d29e4d2
Wesson <vinokurov.filipp1@yandex.ru>
2022-09-08 06:19:19 +0300

    made minor edits in styling

--- static/js/app_cart/successful-order-popup.js
+++ static/js/app_cart/successful-order-popup.js
@@ -1,12 +1,16 @@
 let popup = document.getElementById("address__order-popup");
 let popupWindow = document.getElementById("address__popup-window");
+let body = document.body
 
-window.onload = function openPopup() {
+function openPopup() {
     popup.classList.add("order-popup-active");
     popupWindow.classList.add("popup-window-active");
+    body.classList.add("none-scroll-body")
 }
 
 function closePopup(){
     popup.classList.remove("order-popup-active");
     popupWindow.classList.remove("popup-window-active");
 }
+
+window.onload = setTimeout(openPopup, 1000);
