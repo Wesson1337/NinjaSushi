0675e5fac5f11116c21bbe950e2d38dfb140f860
Wesson <vinokurov.filipp1@yandex.ru>
2022-09-18 15:26:16 +0300

    added class 'input-field' in login and registration form fields

--- app_accounts/forms.py
+++ app_accounts/forms.py
@@ -7,16 +7,16 @@ from app_accounts.models import CustomUser
 class LoginForm(AuthenticationForm):
     def __init__(self, *args, **kwargs):
         super(LoginForm, self).__init__(*args, **kwargs)
-
-    username = UsernameField(widget=forms.TextInput(
-        attrs={'class': 'input-field', 'id': 'username-input'}
-    ))
-    password = forms.CharField(widget=forms.PasswordInput(
-        attrs={'class': 'input-field', 'id': 'password-input'}
-    ))
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'input-field'
 
 
 class RegistrationForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super(RegistrationForm, self).__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'input-field'
+
     class Meta:
         model = CustomUser
         fields = ('username', 'first_name', 'phone_number', 'date_of_birth', 'password1', 'password2')
