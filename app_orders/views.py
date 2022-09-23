2ddc67527a3476578c3105154a16ee294f6aa7a2
Wesson <vinokurov.filipp1@yandex.ru>
2022-09-23 04:07:51 +0300

    added context in order history detail view

--- app_orders/views.py
+++ app_orders/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.views.generic import DetailView
-from app_orders.models import Order
+from app_orders.models import Order, OrderItem
 
 
 class OrderHistoryDetailView(DetailView):
+    model = Order
     template_name = 'app_orders/order_history_detail_page.html'
-    queryset = Order.objects.all()
     context_object_name = 'order'
+
+    def get_context_data(self, **kwargs):
+        order_items = OrderItem.objects.select_related('product').filter(order=self.kwargs.get('pk'))
+        context = {'order_items': order_items}
+        return context
