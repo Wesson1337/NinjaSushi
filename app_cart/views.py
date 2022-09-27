b7f92352e002a6e7257b21e8aed28db75728412c
Wesson <vinokurov.filipp1@yandex.ru>
2022-09-27 03:36:53 +0300

    made autocompletion of the order form depending on the previous order

--- app_cart/views.py
+++ app_cart/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
 
 from app_orders.tasks import order_created_email_task
-from app_orders.models import OrderItem
+from app_orders.models import OrderItem, Order
 from app_shop.models import Product
 from app_cart.cart import Cart
 from django.views import View
@@ -48,7 +48,11 @@ class CartView(View):
     """View for rendering and post-processing cart_page.html"""
     def get(self, request) -> HttpResponse:
         cart = Cart(request)
-        return render(request, 'app_cart/cart_page.html', context={'cart': cart})
+        context = {'cart': cart}
+        if request.user.is_authenticated:
+            last_order = Order.objects.filter(user=request.user).last()
+            context['last_order'] = last_order
+        return render(request, 'app_cart/cart_page.html', context=context)
 
     def post(self, request) -> HttpResponseRedirect:
         cart = Cart(request)
